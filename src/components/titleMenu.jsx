5b258e1c05e69ed57ec3e292beb52bab640f8537
anhdddd <tranngoctin77@gmail.com>
2024-01-05 17:41:11 +0700

    change icon

--- src/components/titleMenu.jsx
+++ src/components/titleMenu.jsx
@@ -1,18 +1,42 @@
 import { Menu } from "lucide-react";
 import { useOpenUtilityBarStore } from "@/stores/useOpenUtilityBar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export function TitleMenu(props) {
   const { isOpen, setUtilityBar } = useOpenUtilityBarStore();
   const handleClick = () => {
-      setUtilityBar(!isOpen);
-      console.log(!isOpen);
+    setUtilityBar(!isOpen);
+    console.log(!isOpen);
   };
   return (
-      <div className="fixed top-0 flex px-3 items-center z-10 w-full bg-white py-3.5">
-          <Menu onClick={handleClick} className="w-7 h-7 p-1.5 rounded-lg bg-gray-200 transition duration-150 hover:bg-gray-600 hover:text-white" />
-          <h2 className="px-4 text-lg font-semibold tracking-tight select-none">
-              {props.title}
-          </h2>
+    <div className="fixed top-0 flex px-3 w-full items-center z-10 bg-white py-3.5">
+      <div className="flex">
+        <Menu
+          onClick={handleClick}
+          className="w-7 h-7 p-1.5 rounded-lg bg-gray-200 transition duration-150 hover:bg-gray-600 hover:text-white"
+        />
+        <h2 className="px-4 text-lg font-semibold tracking-tight select-none">
+          {props.title}
+        </h2>
       </div>
+      <div className="fixed top-3 right-7 h-8 w-8 cursor:pointer">
+        <Popover className="rounded-md ">
+          <PopoverTrigger >
+            <Avatar className="h-8 w-8 ring-offset-1 transition-all duration-300 hover:ring-4">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="w-40 shadow flex flex-col">
+                Hieenr thij giao dien, vaf button logout
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
   );
 }
